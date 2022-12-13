81c57e47e07325817ae48fc81d452f88e6265446
phan chau <phanchau66@gmail.com>
2022-12-13 09:26:33 +0700

    hello

--- js/typeahead.js
+++ js/typeahead.js
@@ -22,16 +22,7 @@
     };
   };
 
-  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
+  var states = ["10a1","10a2","10a3","10a4","10a5","10a6","10a7","10a8","10a9","10a10","11a1","11a2","11a3","11a4","11a5","11a6","11a7","11a8","11a9","11a10","12a1","12a2","12a3","12a4","12a5","12a6","12a7","12a8","12a9","12a10"];
 
   $('#the-basics .typeahead').typeahead({
     hint: true,
